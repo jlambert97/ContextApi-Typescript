db0910028fb3ed6f6ae275c418c08ffcfab5094c
jlambert97 <joao.lambert@outlook.com>
2019-08-21 23:03:29 -0300

    Ajustes Basicos

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import Context from '../../App/Context'
+import { Container } from './style'
 const Home = () => {
+    const { state, dispatch } = useContext(Context)
     return(
-        <div>Xaxa</div>
+        <Container>
+            <div>{state.value}</div>
+            <button 
+                onClick={() => dispatch({type: 'SET_VALUE', payload:'Teste'})}
+            >Template Dispatch</button>
+        </Container>
     )
 }
 
